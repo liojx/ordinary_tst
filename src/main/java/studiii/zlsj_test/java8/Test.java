5f79d4017662d3061b97fc6865e2a1fb8f9e5167
liaodaye <liao_2464@163.com>
2018-09-03 11:26:01 +0800

    tijiao

--- src/main/java/studiii/zlsj_test/java8/Test.java
+++ src/main/java/studiii/zlsj_test/java8/Test.java
@@ -1,6 +1,10 @@
 package studiii.zlsj_test.java8;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import studiii.zlsj_test.pojo.DBPO;
 
 /**
  * Author: liaosijun
@@ -10,17 +14,42 @@ import java.util.ArrayList;
  */
 public class Test {
     public static void main(String[] args) {
-        ArrayList<String> arr = new ArrayList<String>();
-        for(int i = 0; i <= 10;i++){
-            arr.add(i+"");
-        }
-        arr.forEach(s -> {
-            System.out.println(s);
-        });
-
-        arr.forEach(Test :: pri);
+//        ArrayList<String> arr = new ArrayList<String>();
+//        for(int i = 0; i <= 10;i++){
+//            arr.add(i+"");
+//        }
+//        arr.forEach(s -> {
+//            System.out.println(s);
+//        });
+//
+//        arr.forEach(Test :: pri);
+        abc();
     }
     static void pri(String s){
         System.out.println(s);
+    }
+    
+    static void abc() {
+    	List<DBPO> userProps = new ArrayList<DBPO>();
+    	DBPO po1 = new DBPO();
+    	po1.setUrl("/a");
+    	po1.setPwd("123");
+    	po1.setUserName("ls");
+    	DBPO po2 = new DBPO();
+    	po2.setUrl("/b");
+    	po2.setPwd("123");
+    	po2.setUserName("ls");
+    	DBPO po3 = new DBPO();
+    	po3.setUrl("/c");
+    	po3.setPwd("123");
+    	po3.setUserName("ls");
+    	
+    	userProps.add(po1);
+    	userProps.add(po2);
+    	userProps.add(po3);
+    	
+    	List<String> stf = userProps.stream().map(st ->st.getUrl()).collect(Collectors.toList());
+    	stf.forEach(Test :: pri);
+    	
     }
 }
