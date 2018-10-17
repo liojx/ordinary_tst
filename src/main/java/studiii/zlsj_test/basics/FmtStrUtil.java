b086bc12990e60db70eefbc5a4fd1d461be5202a
liaosijun <liaosijun@doctorwork.com>
2018-10-17 14:45:26 +0800

    未完成  fmtStr

---
+++ src/main/java/studiii/zlsj_test/basics/FmtStrUtil.java
@@ -0,0 +1,27 @@
+package studiii.zlsj_test.basics;
+
+import java.io.File;
+
+/**
+ * @author liaosijun
+ * @since 2018年10月17日 上午10:50:07
+ */
+public class FmtStrUtil {
+	
+	private static String path = "‪c:/tmmp/"; // 存放txt文件路径
+	
+	private static String fileName = "fmt";
+	
+	private static String suffix = ".txt";
+	
+	private static void xSpaceLine() {
+		String p = path + fileName + suffix;
+		System.out.println(p);
+		File file = new File("‪c:/tmmp/fmt.txt");
+		System.out.println(file.exists());
+	}
+	
+	public static void main(String[] args) {
+		xSpaceLine();
+	}
+}
