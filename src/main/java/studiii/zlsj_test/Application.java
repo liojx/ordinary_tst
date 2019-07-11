f3401fb33943e7caf98aee0b1303c09e437f937b
liaosijun <liaosijun@evercreative.com.cn>
2019-07-11 09:12:45 +0800

    ....

--- src/main/java/studiii/zlsj_test/Application.java
+++ src/main/java/studiii/zlsj_test/Application.java
@@ -3,18 +3,20 @@ package studiii.zlsj_test;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @EnableSwagger2 // swagger 生效
 @SpringBootApplication(scanBasePackages = "studiii")
 @MapperScan(value = "studiii.zlsj_test.*") // 扫描mybatis xml文件目录
-public class Application  {
+public class Application extends SpringBootServletInitializer {
 
-//	@Override
-//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-//		return application.sources(Application.class);
-//	}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
 
 	public static void main(String[] args) {
 		 SpringApplication.run(Application.class, args);
